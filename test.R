c57175fc705414a9b9739b71d835fb9ecbbbd1b8
Noah McMahon <nam1414@uw.edu>
2025-03-01 19:45:12 -0800

    data importing and cleaning.

--- test.R
+++
@@ -1,9 +0,0 @@
-# This is just a test file to see if the git stuff works
-
-library(tidyverse)
-library(ggplot2)
-install.packages("corrplot")
-library(corrplot)
-
-red_wines <- read.csv2("winequality-red.csv")
-white_wines <- read.csv2("winequality-white.csv")
