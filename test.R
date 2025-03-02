7cde5ca67cd4478c45bb6dd0a329d0cc8c10ce4a
Noah McMahon <nam1414@uw.edu>
2025-03-01 19:35:27 -0800

    adding datasets

--- test.R
+++ test.R
@@ -1,1 +1,9 @@
 # This is just a test file to see if the git stuff works
+
+library(tidyverse)
+library(ggplot2)
+install.packages("corrplot")
+library(corrplot)
+
+red_wines <- read.csv2("winequality-red.csv")
+white_wines <- read.csv2("winequality-white.csv")
